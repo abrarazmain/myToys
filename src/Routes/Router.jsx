b1f420976405504fa568e4ad1dacb3f7ae8ae018
abrarazmain <ajmainabrar5@gmail.com>
2023-05-18 21:50:19 +0600

    complated component setup

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-5xl">Hello world!</div>,
+    },
+]);
+  
+export default router
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <button className="btn">Button</button>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
