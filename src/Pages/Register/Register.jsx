6d39bc12b272344b0361ead24c959f9b66038253
abrarazmain <ajmainabrar5@gmail.com>
2023-05-19 17:45:10 +0600

    added register

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+
+
+const Register = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+          <div className="mt-12 flex flex-col items-center">
+            <h1 className="text-2xl xl:text-3xl font-extrabold">Register</h1>
+            <div className="w-full flex-1 mt-8">
+              <div className="mx-auto max-w-xs">
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  type="text"
+                  placeholder="Name"
+                />
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="email"
+                  placeholder="Email"
+                />
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="password"
+                  placeholder="Password"
+                />
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="text"
+                  placeholder="PhotoURl"
+                />
+                <button className="mt-5 tracking-wide font-semibold bg-[#b18aff] text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                  <svg
+                    className="w-6 h-6 -ml-2 "
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                    <circle cx="8.5" cy={7} r={4} />
+                    <path d="M20 8v6M23 11h-6" />
+                  </svg>
+                  <span className="ml-3">Register</span>
+                </button>
+                <p className="mt-6 text-sm text-gray-600 text-center">
+                  Already have an account? <Link className="font-bold text-[#b18aff]" to='/login'>Login.</Link>
+                 
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 bg-[#ac9fc7] text-center hidden lg:flex ">
+          <div
+            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat "
+            style={{
+              backgroundImage:
+                'url("https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg")',
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
